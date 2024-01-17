6f032fc57238a0fa2f674b6a07ae7fb8f530886b
Jonas Shoukri <116384674+JonaShoukri@users.noreply.github.com>
2024-01-17 16:30:00 -0500

    Done

---
+++ CompoundingCalculaor/CompoundingCalculaor/CompoundingCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace CompoundingCalculaor
+{
+	public class Calculator
+	{
+        byte years;
+        decimal principle;
+        decimal returnRate;
+        decimal yearlyDeposit;
+
+        byte yearsPast;
+        decimal totalInvested;
+        decimal balance;
+
+        public Calculator(byte years=10, decimal principle=100000, decimal returnRate=7, decimal yearlyDeposit=10000)
+		{
+            this.years = years;
+            this.principle = principle;
+            this.returnRate = returnRate;
+            this.yearlyDeposit = yearlyDeposit;
+            yearsPast = 0;
+            totalInvested = principle;
+            balance = principle;
+		}
+
+        public void Calculate()
+        {
+            while (yearsPast < years)
+            {
+                totalInvested += yearlyDeposit;
+                balance = (balance * (1 + returnRate / 100)) + yearlyDeposit;
+                yearsPast++;
+                Write();
+            }
+        }
+
+        public void Write()
+        {
+            Console.WriteLine($"Year #{yearsPast}\n" +
+                $"Total Invested: {totalInvested}\n" +
+                $"Profit %: {(100 * balance) / totalInvested }\n" +
+                $"Balance: {balance}\n");
+        }
+	}
+}
+
---
+++ CompoundingCalculaor/CompoundingCalculaor/Program.cs
@@ -0,0 +1,15 @@
+﻿using CompoundingCalculaor;
+
+/* 
+ * This example simulates the compounding of an account with initial value of 100K at 7% per year for 34 years to which we add an extra 10k per year.
+ * 
+ * years = how many years you plan to invest (example 34)
+ * principle = initial deposit (example 100k)
+ * returnRate = the amount, in %, the investement in questions returns per year (example 7%)
+ * yearlyDeposit = the amount of money you plan to add to this account yearly (example 10k)
+ */
+
+Calculator calculator = new Calculator(34, 10000, 7, 10000); 
+
+calculator.Calculate();
+Console.ReadKey();
